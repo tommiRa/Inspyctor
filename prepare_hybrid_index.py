70ba0f3d150d888ef52dddec7f2b50468cc1313e
tommiRa <tommitr@gmail.com>
2018-02-06 11:35:22 +0200

    Update prepare_hybrid_index.py

--- prepare_hybrid_index.py
+++ prepare_hybrid_index.py
@@ -6,7 +6,8 @@ import shutil
 
 parser = argparse.ArgumentParser(description='Generate index files for BWA')
 parser.add_argument('-g','--genome', dest="genome", help="The genomic sequence in fasta format")
-parser.add_argument('-p','--plasmid', dest="plasmid", help="The plasmid sequence in fasta format")
+parser.add_argument('-i','--insert', dest="insert", help="The insert sequence in fasta format")
+parser.add_argument('-h','--insert_header', dest="insert_header", help="The header used to identify the inserted sequence")
 parser.add_argument('-o','--outputdir', dest="outputdir", help="The output directory")
 args = parser.parse_args()
 
@@ -18,14 +19,26 @@ def main():
 		os.mkdir(outdir)
 	else:
 		pass
+	print("Changing the fasta header for insert fasta file")
+	insert_fasta = open(args.plasmid)
+	temp_fasta = open("temp_insert.fa","w")
+	for i,ln in enumerate(insert_fasta.readlines()):
+		if i == 0:
+			temp_fasta.write(">{0}".format(args.insert_header))
+		else:
+			temp_fasta.write(ln)
+	insert_fasta.close()
+	temp_fasta.close()
 	print("Merging fasta files")
-	proc = subprocess.Popen(["cat", args.genome, args.plasmid], stdout = open(outdir + "/hybrid_genome.fa", "w"))
+	proc = subprocess.Popen(["cat", args.genome, "temp_insert.fa"], stdout = open(outdir + "/hybrid_genome.fa", "w"))
 	proc.wait()
 	print("Finished merging fasta files")
 	# Call BWA to create the index 
 	print("Creating index")
 	proc2 = subprocess.Popen(["bwa", "index", "-a", "bwtsw", outdir + "/hybrid_genome.fa"])
 	proc2.wait()
+	print("Cleaning up")
+	os.remove("temp_insert.fa")
 	print("Finished!")
 
 if __name__ == "__main__":
